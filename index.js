cd89ffa0b872446027d44a864487c60d76b7ddb2
ivan_kondratev <ivanxab54@gmail.com>
2025-01-14 21:22:52 +0700

    install express.js, created docker class

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import { createServer } from 'node:http'
-import dockerExecCommand from './docker.js'
+import Docker from '.lib/docker.js'
+import express from 'express'
 
-const server = createServer(async (req, res) => {
-  res.end(await dockerExecCommand(req.url))
+const app = express()
+const docker = new Docker('/var/run/docker.sock')
+const port = 3000
+
+app.get('/', async (req, res) => {
+  res.send(await docker.getVersion())
 })
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000')
+app.get('/images', async (req, res) => {
+  res.send(await docker.getAllImages())
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:3000`)
 })
