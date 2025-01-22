ab36a6907521235d423e12b7d0d7db5770e3f0f1
ivan <ivanxab54@gmail.com>
2025-01-22 23:10:45 +0700

    enable cors, added endpoint for getting docker containers

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import Docker from '.lib/docker.js'
+import Docker from './lib/docker.js'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const docker = new Docker('/var/run/docker.sock')
 const port = 3000
+
+app.use(cors({
+   origin: 'http://localhost:5173'
+}))
 
 app.get('/', async (req, res) => {
   res.send(await docker.getVersion())
@@ -12,6 +17,11 @@ app.get('/', async (req, res) => {
 app.get('/images', async (req, res) => {
   res.send(await docker.getAllImages())
 })
+
+app.get('/containers', async (req, res) => {
+  res.send(await docker.getAllContainers())
+})
+
 
 app.listen(port, () => {
   console.log(`http://localhost:3000`)
